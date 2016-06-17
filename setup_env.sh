638dea322ed3aefbf58ba0341d1d35c3dcc62624
Hoang Trung Huy <huyht1205@gmail.com>
2016-06-17 08:50:19 +0700

    setup_env: newly created
    Signed-off-by: Hoang Trung Huy <huyht1205@gmail.com>

---
+++ setup_env.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function main() {
+	git_config
+	git_alias
+	initScript_copy bash_aliases
+	initScript_copy vimrc
+}
+
+function git_config() {
+	printf "${FUNCNAME}($1)..."
+	git config --global user.name "Hoang Trung Huy"
+	git config --global user.email "huyht1205@gmail.com"
+	git config --global core.editor "vim"
+	printf "finished.\n"
+}
+
+function git_alias() {
+	printf "${FUNCNAME}($1)..."
+	git config --global alias.s status
+	git config --global alias.l log
+	git config --global alias.b branch
+	printf "finished.\n"
+}
+
+function initScript_copy() {
+	printf "${FUNCNAME}($1)..."
+	TARGET=$1
+	if [[ -e ./TARGET ]]; then
+		cp ./TARGET ~/.TARGET
+		printf "finished.\n"
+	else
+		printf "\e[31mERROR: File not found!\e[0m\n"
+	fi
+}
+
+main "$@"
