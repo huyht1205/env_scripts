bf8227072ad38dd554ba848d7a81e13b05ac6732
Hoang Trung Huy <huyht1205@gmail.com>
2016-06-18 11:28:24 -0700

    linux_build_ros_indigo: update
    Signed-off-by: Hoang Trung Huy <huyht1205@gmail.com>

--- linux_build_ros_indigo.sh
+++ linux_build_ros_indigo.sh
@@ -5,7 +5,7 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
 
 sudo apt-get update
-sudo apt-get install ros-indigo-ros-base python-rosinstall
+sudo apt-get install -y ros-indigo-ros-base python-rosinstall 
 
 sudo rosdep init
 rosdep update
