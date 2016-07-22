2ee73eee1ca597ce0d9b03600f73a551995ee0b5
Hoang Trung Huy <huyht1205@gmail.com>
2016-07-22 15:13:51 +0700

    update
    Signed-off-by: Hoang Trung Huy <huyht1205@gmail.com>

--- automation/ros_install/Build.sh
+++ automation/ros_install/Build.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-if [[ -f ./rostoolchain.cmake ]]
-then
-	./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=./rostoolchain.cmake
-else
-	./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release
-fi
+./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/workspace/ros/arm_ros_catkin_ws/rostoolchain.cmake
