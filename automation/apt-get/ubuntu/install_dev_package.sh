4e88a9dd0bc1038205354caf7e360986474173fe
Hoang Trung Huy <huyht1205@gmail.com>
2016-12-02 17:39:21 +0700

    update autotools
    Signed-off-by: Hoang Trung Huy <huyht1205@gmail.com>

--- automation/apt-get/ubuntu/install_dev_package.sh
+++ automation/apt-get/ubuntu/install_dev_package.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-MY_PKG="openssh-server avahi-daemon avahi-discover avahi-dnsconfd avahi-utils picocom"
+MY_PKG="openssh-server avahi-daemon avahi-discover avahi-dnsconfd avahi-utils picocom ctags"
 YOCTO_DEV="gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat libsdl1.2-dev xterm"
 KERNEL_DEV="u-boot-tools lzop"
+AUTOTOOLS="autoconf automake libtool"
 
 apt-get install ${MY_PKG} ${YOCTO_DEV} ${KERNEL_DEV}
