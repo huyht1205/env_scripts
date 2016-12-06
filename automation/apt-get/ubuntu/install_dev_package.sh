e9020da459d83a454ec043f9aaa230c0968af502
Hoang Trung Huy <huyht1205@gmail.com>
2016-12-06 09:16:36 +0700

    automation: apt-get: ubuntu: install_dev: add sshfs
    Signed-off-by: Hoang Trung Huy <huyht1205@gmail.com>

--- automation/apt-get/ubuntu/install_dev_package.sh
+++ automation/apt-get/ubuntu/install_dev_package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MY_PKG="openssh-server avahi-daemon avahi-discover avahi-dnsconfd avahi-utils picocom ctags"
+MY_PKG="openssh-server avahi-daemon avahi-discover avahi-dnsconfd avahi-utils picocom ctags sshfs"
 YOCTO_DEV="gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat libsdl1.2-dev xterm"
 KERNEL_DEV="u-boot-tools lzop libncurses5 libncurses5-dev"
 AUTOTOOLS="autoconf automake libtool"
