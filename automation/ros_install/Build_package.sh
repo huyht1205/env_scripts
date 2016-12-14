184e9cb12b1d4979ba5b87ac7155376f3a3c21d6
Hoang Trung Huy <huyht1205@gmail.com>
2016-12-14 11:17:44 +0700

    clean up
    Signed-off-by: Hoang Trung Huy <huyht1205@gmail.com>

--- automation/ros_install/Build_package.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-../src/catkin/bin/catkin_make_isolated -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/Users/hoangtrunghuy/Desktop/workspace/arm_ros_catkin_ws/rostoolchain.cmake
--- automation/ros_install/Init.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rosinstall_generator ros_comm --rosdistro indigo --deps --wet-only --tar > ros_comm-wet.rosinstall
-wstool init -j8 src ros_comm-wet.rosinstall
