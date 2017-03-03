83ceb201b7d02a6362063273b7e294901d2cc2ef
Luck Hoang <huyht1205@gmail.com>
2017-03-03 17:43:13 +0700

    Linux: etc: remote sources.list common
    Signed-off-by: Luck Hoang <huyht1205@gmail.com>

--- automation/package-manager/ubuntu/install_dev_package.sh
+++ automation/package-manager/ubuntu/install_dev_package.sh
@@ -21,6 +21,6 @@ AUTOTOOLS=" \
 apt-get install ${MY_PKG} ${YOCTO_DEV} ${KERNEL_DEV}
 
 # Java virtual machin
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer oracle-java8-set-default 
+#sudo add-apt-repository ppa:webupd8team/java
+#sudo apt-get update
+#sudo apt-get install oracle-java8-installer oracle-java8-set-default 
