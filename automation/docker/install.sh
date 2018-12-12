22e3b8a1495c77d4baa70909631d9639dbfe59b7
Hoang Trung Huy <huyht1205@protonmail.com>
2018-12-12 17:59:10 +0900

    docker: add configs
    Signed-off-by: Hoang Trung Huy <huyht1205@protonmail.com>

--- automation/docker/install.sh
+++ automation/docker/install.sh
@@ -23,11 +23,11 @@ function install_deps() {
 }
 
 function install_key() {
-    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+    curl -fsSL https://download.docker.com/linux/${DISTRO}/gpg | sudo apt-key add -
 }
 
 function add_repo() {
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian \
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/${DISTRO} \
     $(lsb_release -cs) stable"
 }
 
@@ -36,12 +36,21 @@ function install_docker() {
     sudo apt install -y docker-ce
 }
 
+function config_docker() {
+    sudo gpasswd -a ${USER} docker
+    mkdir -p /data/docker
+    sudo cp files/docker.service /lib/systemd/system/docker.service
+    sudo systemctl daemon-reload
+    sudo systemctl restart docker
+}
+
 function main() {
     check_input
     install_deps
     install_key
     add_repo
     install_docker
+    config_docker
 }
 
 main
