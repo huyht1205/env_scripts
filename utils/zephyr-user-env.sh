6ed4169337443eb3b6613ccb88b53d34a189c59d
Hoang Trung Huy <huyht1205@protonmail.com>
2019-02-20 10:28:21 +0900

    zephyr env: update rtos path
    Signed-off-by: Hoang Trung Huy <huyht1205@protonmail.com>

--- utils/zephyr-user-env.sh
+++ utils/zephyr-user-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export OSS_PATH=/data/workspace/20-OSS
-export ZEPHYR_BASE=${OSS_PATH}/zephyrproject/zephyr
+export ZEPHYR_BASE=${OSS_PATH}/rtos/zephyrproject/zephyr
 export ZEPHYR_TOOLCHAIN_VARIANT=gnuarmemb
 export GNUARMEMB_TOOLCHAIN_PATH=/data/sdk/arm-none-eabi-gcc
