598727987d7a91cff90e36e65d7d792d9a152b90
Hoang Trung Huy <huyht1205@protonmail.com>
2019-03-10 11:13:08 +0900

    template: vscode: add defines and includes
    This reverts commit 4ee44837c1fc0efa9b25f6fd26cc819cf325f697.

--- utils/stm32-vscode-include.py
+++ utils/stm32-vscode-include.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 import os, re, sys, json, requests
-C_CPP_JSON=".vscode/c_cpp_properties.json"
-ENV_JSON="/data/git_repo/env_scripts/template/vscode/stm32-env-template.json"
+C_CPP_JSON='.vscode/c_cpp_properties.json'
+ENV_JSON='/data/git_repo/env_scripts/template/vscode/stm32-env-template.json'
+STD_C='c99'
 
 def main(argv):
     global C_CPP_JSON
@@ -18,9 +19,16 @@ def main(argv):
         f.close()
     
     vscodeJson['env'] = envTemplate['env']
+    vscodeConfiguration = vscodeJson['configurations'][0]
+    vscodeConfiguration['compilerPath'] = '${CC}'
+    vscodeConfiguration['cStandard'] = STD_C
 
-    print(vscodeJson)
-    
+    appendConfig = {'includePath': '${uInclude}', 'defines': '${uDefines}'}
+    for key in appendConfig.keys():
+        tmp = vscodeConfiguration[key]
+        if not appendConfig[key] in tmp:
+            tmp.append(appendConfig[key])
+
     with open(C_CPP_JSON, 'w') as out:
         json.dump(vscodeJson, out, sort_keys=True, indent=4)
         out.close()
